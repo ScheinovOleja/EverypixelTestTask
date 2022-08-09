571fdf113925cfa60cec78f09dbe0ad1445c1cb8
oleg <oleg20064@gmail.com>
2022-08-09 12:46:15 +0200

    Сделана базовая структура проекта

---
+++ main.py
@@ -0,0 +1,24 @@
+
+
+
+class Operations:
+
+    def __init__(self):
+        pass
+
+    def resize(self):
+        pass
+
+    def date(self):
+        pass
+
+    def watermark(self):
+        pass
+
+
+async def check_img():
+    pass
+
+
+if __name__ == '__main__':
+    pass
